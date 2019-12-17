88f61f65542367c35a7f69cc291cf9b1f63e8ef5
ibrahimaycan <ibrahimaycan@marun.edu.tr>
2019-12-17 20:28:34 +0300

    Add project files.

---
+++ Client/Client.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    class Client
+    {
+        static Socket socket;
+
+        static void Main(string[] args)
+        {
+            socket = new Socket(AddressFamily.InterNetwork,SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"),1234);
+            try
+            {
+                socket.Connect(localEndPoint);
+
+            }
+            catch
+            {
+                Console.WriteLine("Unable to Connect");
+                Main(args);
+            }
+            int i = 0;
+            while (i<3) {
+                Console.WriteLine("Enter Preferred Hotel");
+                string hotelName = Console.ReadLine();
+                byte[] hotelNamedata = Encoding.ASCII.GetBytes(hotelName);
+
+                Console.WriteLine("Enter Preferred Airline");
+                string AirlineName = Console.ReadLine();
+                byte[] airlineNameData = Encoding.ASCII.GetBytes(AirlineName);
+
+                Console.WriteLine("Enter Date in format ddmmyyyy");
+                string dateTime = Console.ReadLine();
+                byte[] dateTimeData = Encoding.ASCII.GetBytes(dateTime);
+
+                Console.WriteLine("Enter number of Customer");
+                string customerNumber = Console.ReadLine();
+                byte[] data = Encoding.ASCII.GetBytes(hotelName + " " + AirlineName +
+                    " " + dateTime + " " + customerNumber);
+
+                socket.Send(data);
+                Console.Write("data send\r\n");
+                i++;
+            }
+            Console.Read();
+            socket.Close();
+
+                
+            
+        }
+    }
+}
---
+++ Server/Server.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server
+{
+    class Server
+    {
+        static byte[] buffer { get; set; }
+        static Socket socket;
+
+        static void Main(string[] args)
+        {
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.Bind(new IPEndPoint(0, 1234));
+            socket.Listen(100);
+            Socket accepted = socket.Accept();
+
+            int j = 0;
+            while (j < 3) {
+
+ 
+                buffer = new byte[accepted.SendBufferSize];
+                int bytesRead = accepted.Receive(buffer);
+                byte[] formatted = new byte[bytesRead];
+                for (int i = 0; i < bytesRead; i++)
+                {
+                    formatted[i] = buffer[i];
+
+                }
+                string strData = Encoding.ASCII.GetString(formatted);
+                Console.WriteLine(strData);
+                j++;
+            }
+            socket.Close();
+            accepted.Close();
+
+
+        }
+    }
+}
