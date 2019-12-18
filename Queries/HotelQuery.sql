d6c4b43bd1bf8b3e8f8b204e8a82eaa6e73fa31c
ibrahimaycan <ibrahimaycan@marun.edu.tr>
2019-12-18 22:18:55 +0300

    Database queries added

---
+++ Queries/HotelQuery.sql
@@ -0,0 +1,37 @@
+create database Hotel
+create table Hotel_table(
+Hotel_ID int primary key,
+Hotel_Name varchar(50),
+Hotel_trip_date varchar(50),
+Hotel_capasity int not null,
+available_Room int not null,
+);
+insert into Hotel_table(Hotel_Name,Hotel_trip_date,Hotel_capasity,available_Room) values('HILTON','25/12/2019',100,100);
+insert into Hotel_table(Hotel_Name,Hotel_trip_date,Hotel_capasity,available_Room) values('HILTON','26/12/2019',100,100);
+insert into Hotel_table(Hotel_Name,Hotel_trip_date,Hotel_capasity,available_Room) values('HILTON','27/12/2019',100,100);
+insert into Hotel_table(Hotel_Name,Hotel_trip_date,Hotel_capasity,available_Room) values('HILTON','28/12/2019',100,100);
+insert into Hotel_table(Hotel_Name,Hotel_trip_date,Hotel_capasity,available_Room) values('HILTON','29/12/2019',100,100);
+insert into Hotel_table(Hotel_Name,Hotel_trip_date,Hotel_capasity,available_Room) values('HILTON','30/12/2019',100,100);
+insert into Hotel_table(Hotel_Name,Hotel_trip_date,Hotel_capasity,available_Room) values('HILTON','31/12/2019',100,100);
+insert into Hotel_table(Hotel_Name,Hotel_trip_date,Hotel_capasity,available_Room) values('AIRBNB','25/12/2019',100,100);
+insert into Hotel_table(Hotel_Name,Hotel_trip_date,Hotel_capasity,available_Room) values('AIRBNB','26/12/2019',100,100);
+insert into Hotel_table(Hotel_Name,Hotel_trip_date,Hotel_capasity,available_Room) values('AIRBNB','27/12/2019',100,100);
+insert into Hotel_table(Hotel_Name,Hotel_trip_date,Hotel_capasity,available_Room) values('AIRBNB','28/12/2019',100,100);
+insert into Hotel_table(Hotel_Name,Hotel_trip_date,Hotel_capasity,available_Room) values('AIRBNB','29/12/2019',100,100);
+insert into Hotel_table(Hotel_Name,Hotel_trip_date,Hotel_capasity,available_Room) values('AIRBNB','30/12/2019',100,100);
+insert into Hotel_table(Hotel_Name,Hotel_trip_date,Hotel_capasity,available_Room) values('AIRBNB','31/12/2019',100,100);
+insert into Hotel_table(Hotel_Name,Hotel_trip_date,Hotel_capasity,available_Room) values('DIVAN','25/12/2019',100,100);
+insert into Hotel_table(Hotel_Name,Hotel_trip_date,Hotel_capasity,available_Room) values('DIVAN','26/12/2019',100,100);
+insert into Hotel_table(Hotel_Name,Hotel_trip_date,Hotel_capasity,available_Room) values('DIVAN','27/12/2019',100,100);
+insert into Hotel_table(Hotel_Name,Hotel_trip_date,Hotel_capasity,available_Room) values('DIVAN','28/12/2019',100,100);
+insert into Hotel_table(Hotel_Name,Hotel_trip_date,Hotel_capasity,available_Room) values('DIVAN','29/12/2019',100,100);
+insert into Hotel_table(Hotel_Name,Hotel_trip_date,Hotel_capasity,available_Room) values('DIVAN','30/12/2019',100,100);
+insert into Hotel_table(Hotel_Name,Hotel_trip_date,Hotel_capasity,available_Room) values('DIVAN','31/12/2019',100,100);
+insert into Hotel_table(Hotel_Name,Hotel_trip_date,Hotel_capasity,available_Room) values('SWISS','25/12/2019',100,100);
+insert into Hotel_table(Hotel_Name,Hotel_trip_date,Hotel_capasity,available_Room) values('SWISS','26/12/2019',100,100);
+insert into Hotel_table(Hotel_Name,Hotel_trip_date,Hotel_capasity,available_Room) values('SWISS','27/12/2019',100,100);
+insert into Hotel_table(Hotel_Name,Hotel_trip_date,Hotel_capasity,available_Room) values('SWISS','28/12/2019',100,100);
+insert into Hotel_table(Hotel_Name,Hotel_trip_date,Hotel_capasity,available_Room) values('SWISS','29/12/2019',100,100);
+insert into Hotel_table(Hotel_Name,Hotel_trip_date,Hotel_capasity,available_Room) values('SWISS','30/12/2019',100,100);
+insert into Hotel_table(Hotel_Name,Hotel_trip_date,Hotel_capasity,available_Room) values('SWISS','31/12/2019',100,100);
+
